ec4ff72c327b59ac011afa39074b2c3d2c714d6a
Giridhar Achuthananda <141910777+giridhar1103@users.noreply.github.com>
2026-04-26 10:51:56 -0700

    Add Terraform IAM and Glue modules

---
+++ infra/modules/iam/main.tf
@@ -0,0 +1,72 @@
+resource "aws_iam_role" "glue" {
+  name = "${var.project_name}-glue-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "glue.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "glue" {
+  name = "${var.project_name}-glue-policy"
+  role = aws_iam_role.glue.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject", "s3:ListBucket"]
+        Resource = [var.bucket_arn, "${var.bucket_arn}/*"]
+      },
+      {
+        Effect = "Allow"
+        Action = ["glue:*", "logs:*", "cloudwatch:PutMetricData"]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "ec2" {
+  name = "${var.project_name}-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "ec2" {
+  name = "${var.project_name}-ec2-policy"
+  role = aws_iam_role.ec2.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = ["s3:GetObject", "s3:PutObject", "s3:ListBucket"]
+        Resource = [var.bucket_arn, "${var.bucket_arn}/*"]
+      },
+      {
+        Effect = "Allow"
+        Action = ["cloudwatch:PutMetricData", "logs:CreateLogStream", "logs:PutLogEvents"]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2" {
+  name = "${var.project_name}-ec2-profile"
+  role = aws_iam_role.ec2.name
+}
